f17640264731d929ef8e21a1f2a76750a3089b29
jonariv09 <ingjoseagustinnarvaez@gmail.com>
2019-11-10 07:46:42 -0600

    Login validation

--- Startup.cs
+++ Startup.cs
@@ -7,8 +7,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using OnyxPlataform.Models;
 
 namespace OnyxPlataform
 {
@@ -31,7 +33,8 @@ namespace OnyxPlataform
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
+            services.AddDbContext<AplicationDbContext>(options =>
+                options.UseMySql(Configuration.GetConnectionString("OnyxPlataformContext")));
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
@@ -52,8 +55,8 @@ namespace OnyxPlataform
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseAuthentication();
             app.UseCookiePolicy();
+            app.UseAuthentication();
 
             app.UseMvc(routes =>
             {
