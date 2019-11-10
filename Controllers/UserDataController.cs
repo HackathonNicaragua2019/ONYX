b3d7a79339db3ce154e61bc16b0ac50f09e1750f
jonariv09 <ingjoseagustinnarvaez@gmail.com>
2019-11-10 01:18:11 -0600

    Adding items carrousel

--- Controllers/UserDataController.cs
+++ Controllers/UserDataController.cs
@@ -1,13 +1,8 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
 using OnyxPlataform.Models;
-using System.Security.Claims;
 
 namespace OnyxPlataform.Controllers
 {
@@ -16,7 +11,7 @@ namespace OnyxPlataform.Controllers
         //readonly data
         public readonly AplicationDbContext _Context;
         //constructor
-        public UserDataController(AplicationDbContext context){
+        public UserDataController(AplicationDbContext context) {
             _Context=context;
         }
         //index de usuarios
